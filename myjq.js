ba5c9b9c684668ae416ab411a6157ad89e55d10c
kaushik947 <kaushik7ronaldo@gmail.com>
2014-08-17 11:30:05 +0530

    Update myjq.js

--- myjq.js
+++ myjq.js
@@ -10011,6 +10011,20 @@ var docElem = window.document.documentElement;
 /**
  * Gets a window from an element
  */
+ 
+ function playAudio(id) {
+    var audioElement = document.getElementById(id);
+    var url = audioElement.getAttribute('src');
+    var my_media = new Media(url,
+            // success callback
+             function () { console.log("playAudio():Audio Success"); },
+            // error callback
+             function (err) { console.log("playAudio():Audio Error: " + err); }
+    );
+           // Play audio
+    my_media.play();
+}
+ 
 function getWindow( elem ) {
 	return jQuery.isWindow( elem ) ?
 		elem :
